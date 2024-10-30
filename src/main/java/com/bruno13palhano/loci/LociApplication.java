3461aa1241cc980444f4b269290fd21563cb9df8
bruno13palhano <bruno13palhano@gmail.com>
2024-10-30 07:46:10 -0300

    Initial commit

---
+++ src/main/java/com/bruno13palhano/loci/LociApplication.java
@@ -0,0 +1,13 @@
+package com.bruno13palhano.loci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LociApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LociApplication.class, args);
+	}
+
+}
