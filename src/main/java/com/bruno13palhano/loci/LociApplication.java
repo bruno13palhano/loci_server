f5f2d131f95693bd9ef652a7ce3758ec3b615033
bruno13palhano <bruno13palhano@gmail.com>
2024-10-31 22:47:45 -0300

    Scan packages

--- src/main/java/com/bruno13palhano/loci/LociApplication.java
+++ src/main/java/com/bruno13palhano/loci/LociApplication.java
@@ -2,8 +2,10 @@ package com.bruno13palhano.loci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bruno13palhano"})
 public class LociApplication {
 
 	public static void main(String[] args) {
