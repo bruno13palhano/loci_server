56158fec52420ef2b6a4ca5017c44ac2d6b845ea
bruno13palhano <bruno13palhano@gmail.com>
2024-11-01 09:16:22 -0300

    Add UserService interface

---
+++ core/src/main/java/com/bruno13palhano/data/service/UserService.java
@@ -0,0 +1,7 @@
+package com.bruno13palhano.data.service;
+
+import com.bruno13palhano.User;
+import com.bruno13palhano.data.shared.UserData;
+
+public interface UserService extends UserData<User> {
+}
